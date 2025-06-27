f9245ed63f4a488fc49a92c8095380338b3bda49
Duy-2706 <hoangkhacduy2004@gmail.com>
2025-06-28 01:22:11 +0700

    Thay đoi 28/6

--- views/loaitaisans/create.php
+++ views/loaitaisans/create.php
@@ -4,7 +4,7 @@
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item"><a href="index.php?model=loaitaisan&action=index">Loại Tài Sản</a></li>
-                    <li class="breadcrumb-item active" aria-current="page">Tạo Loại Tài Sản</li>
+                    <li class="breadcrumb-item active" aria-current="page">Tạo Loại tài sản </li>
                 </ol>
             </nav>
         </div>
